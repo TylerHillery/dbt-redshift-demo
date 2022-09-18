31d4706dc191d58e40a8336f50772e44907b3ba8
Tyler Hillery <tyler.p.hillery@outlook.com>
2022-09-18 20:57:19 +0000

    Added staging users and venues models

---
+++ models/tickit/staging/stg_tickit__venues.sql
@@ -0,0 +1,21 @@
+with source as (
+    select * from {{ source('tickit', 'venue') }}
+),
+
+renamed as (
+
+    select
+        venueid as venue_id,
+        venuename as venue_name,
+        venuecity as venue_city,
+        venuestate as venue_state,
+        venueseats as venue_seats
+    from
+        source
+    where
+        venue_id IS NOT NULL
+    order by
+        venue_id
+)
+
+select * from renamed
