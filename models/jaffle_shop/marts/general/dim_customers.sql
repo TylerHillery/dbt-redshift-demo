482a4bfb5181bac885df0685fc785751408b7a71
Tyler Hillery <tyler.p.hillery@outlook.com>
2022-09-25 16:43:56 +0000

    Reformatted models to meet style guide

--- models/jaffle_shop/marts/general/dim_customers.sql
+++ models/jaffle_shop/marts/general/dim_customers.sql
@@ -1,4 +1,5 @@
-with customers as (
+with 
+customers as (
     select * from  {{ ref('stg_customers') }}
 ),
 
@@ -7,22 +8,18 @@ orders as (
 ),
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
-    from orders
-
+    from 
+        orders
     group by 1
 
 ),
 
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -30,10 +27,10 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+    from
+        customers
+        left join customer_orders 
+            using (customer_id)
 
 )
 
--- models/jaffle_shop/staging/stg_orders.sql
+++ models/jaffle_shop/staging/stg_orders.sql
@@ -1,7 +1,16 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from
-    {{ source('dbtworkshop', 'jaffle_shop_orders') }}
+with 
+orders as (
+    select * from {{ source('dbtworkshop', 'jaffle_shop_orders') }}
+),
+
+rename as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from
+        orders
+)
+
+select * from rename
